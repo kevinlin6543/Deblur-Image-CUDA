fec7ce2d5ce7923aab09d7876401b6ae6cf4cfb5
Leart Krasniqi <leart.kr@gmail.com>
2020-03-15 19:26:25 -0400

    Added GPU timing metrics

---
+++ src/gpu_time.hpp
@@ -0,0 +1,51 @@
+/* 
+	Header file for GPU timing 
+ 	Based on https://devblogs.nvidia.com/how-implement-performance-metrics-cuda-cc/ 
+
+	Usage:
+		gpu_time gt;
+		gt.begin();
+		[stuff]
+		gt.end();
+		cout << "Elapsed time: " << gt.elap_time() << endl;
+		
+ */
+
+class gpu_time {
+	cudaEvent_t start, stop;
+
+	/* Constructor */
+	gpu_time()
+	{
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+	}
+
+	/* Destructor */
+	~gpu_time()
+	{
+		cudaEventDestroy(start);
+		cudaEventDestroy(stop);
+	}
+
+	/* Begin the timer */
+	void begin()
+	{
+		cudaEventRecord(start, 0);
+	}
+
+	/* Stop the timer */
+	void end()
+	{
+		cudaEventRecord(stop, 0);
+	}
+
+	/* Calculate the total elapsed time */
+	float elap_time()
+	{
+		float t = 0;
+		cudaEventSynchronize(stop);
+		cudaEvenElapsedTime(&t, start, stop);
+		return t;
+	}
+};
